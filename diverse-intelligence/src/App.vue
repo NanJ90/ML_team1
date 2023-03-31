a4443c807e878954a22a2fae6c0586b23be06c83
JJ <njia@gradcenter.cuny.edu>
2023-03-31 10:59:34 -0400

    front-end with Vue.js

---
+++ diverse-intelligence/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <LogIn/>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import LogIn from './components/LogIn.vue'
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    LogIn
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
